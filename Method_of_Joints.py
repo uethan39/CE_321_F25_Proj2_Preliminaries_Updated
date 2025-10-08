3709b265d302c7dd792bf9fcbe9b6156e6c8acd5
uethan39 <uethan@byu.edu>
2025-10-08 17:39:21 -0600

    Add files via upload

--- Method_of_Joints.py
+++ Method_of_Joints.py
@@ -5,6 +5,15 @@ Created on Wed Jul 14 12:37:32 2021
 
 @author: kendrick shepherd
 """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 14 12:37:32 2021
+
+@author: kendrick shepherd
+Updated to fix single- and two-bar force computations
+"""
+
 import sys
 import numpy as np
 import Geometry_Operations as geom
@@ -37,45 +46,57 @@ def NodeIsViable(node):
 
 # Compute unknown force for a single bar at a node
 def SumOfForcesInLocalX(node, bar):
-    sum_x = node.xforce_external + (node.xforce_reaction if not np.isnan(node.xforce_reaction) else 0)
-    sum_y = node.yforce_external + (node.yforce_reaction if not np.isnan(node.yforce_reaction) else 0)
+    vec = geom.BarNodeToVector(node, bar)
+    length = np.linalg.norm(vec)
+    if length == 0:
+        sys.exit("Zero-length bar detected")
+    
+    cos_theta = vec[0] / length
+    sin_theta = vec[1] / length
+
+    sum_fx = node.xforce_external + (node.xforce_reaction if not np.isnan(node.xforce_reaction) else 0)
+    sum_fy = node.yforce_external + (node.yforce_reaction if not np.isnan(node.yforce_reaction) else 0)
 
     for other_bar in node.bars:
-        if other_bar.is_computed:
-            sum_x += other_bar.axial_load * geom.CosineBars(bar, other_bar)
-            sum_y += other_bar.axial_load * geom.SineBars(bar, other_bar)
+        if other_bar.is_computed and other_bar != bar:
+            vec_o = geom.BarNodeToVector(node, other_bar)
+            length_o = np.linalg.norm(vec_o)
+            cos_o = vec_o[0] / length_o
+            sin_o = vec_o[1] / length_o
+            sum_fx += other_bar.axial_load * cos_o
+            sum_fy += other_bar.axial_load * sin_o
 
-    bar.axial_load = -sum_x
+    bar.axial_load = -(sum_fx * cos_theta + sum_fy * sin_theta)
     bar.is_computed = True
 
-# Compute unknown forces for two unknown bars at a node using proper 2x2 system
+# Compute unknown forces for two unknown bars at a node using 2x2 system
 def SumOfForcesInLocalY(node, unknown_bars):
     if len(unknown_bars) != 2:
         sys.exit("SumOfForcesInLocalY requires exactly two unknown bars")
 
     bar1, bar2 = unknown_bars
 
-    # Vectors of each bar from the node
     vec1 = geom.BarNodeToVector(node, bar1)
     vec2 = geom.BarNodeToVector(node, bar2)
 
-    # Cosine and sine of each bar relative to global x-axis
-    cos1 = geom.CosineVectors([1,0], vec1)
-    sin1 = geom.SineVectors([1,0], vec1)
-    cos2 = geom.CosineVectors([1,0], vec2)
-    sin2 = geom.SineVectors([1,0], vec2)
+    # Cosine and sine along global x and y
+    cos1 = vec1[0] / np.linalg.norm(vec1)
+    sin1 = vec1[1] / np.linalg.norm(vec1)
+    cos2 = vec2[0] / np.linalg.norm(vec2)
+    sin2 = vec2[1] / np.linalg.norm(vec2)
 
-    # Sum of known forces at the node
     sum_fx = node.xforce_external + (node.xforce_reaction if not np.isnan(node.xforce_reaction) else 0)
     sum_fy = node.yforce_external + (node.yforce_reaction if not np.isnan(node.yforce_reaction) else 0)
 
-    for bar in node.bars:
-        if bar.is_computed:
-            vec = geom.BarNodeToVector(node, bar)
-            sum_fx += bar.axial_load * geom.CosineVectors([1,0], vec)
-            sum_fy += bar.axial_load * geom.SineVectors([1,0], vec)
+    for other_bar in node.bars:
+        if other_bar.is_computed:
+            vec_o = geom.BarNodeToVector(node, other_bar)
+            length_o = np.linalg.norm(vec_o)
+            cos_o = vec_o[0] / length_o
+            sin_o = vec_o[1] / length_o
+            sum_fx += other_bar.axial_load * cos_o
+            sum_fy += other_bar.axial_load * sin_o
 
-    # Solve the 2x2 linear system
     A = np.array([[cos1, cos2],
                   [sin1, sin2]])
     b = np.array([-sum_fx, -sum_fy])
